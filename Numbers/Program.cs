163b775e7a4106df6c8bdf00a0fa1439564b730c
Merve Karahan <77892947+MerveKarahan@users.noreply.github.com>
2022-01-22 20:56:11 +0300

    min. elemanı attım

--- Numbers/Program.cs
+++ Numbers/Program.cs
@@ -10,16 +10,19 @@ namespace Numbers
             {
                 Array.Sort(arr);
                 Array.Reverse(arr);
-                var orderderArray = new int[arr.Length];
+                var orderderArray = new int[arr.Length-1];
                 var s = 0;
-                var e = arr.Length - 1;
-                for (int i = 0; i < arr.Length; i++)
+                var e = arr.Length - 2; //bu şekilde yaptığımızda en küçük elemanı çıkartabiliriz dizeden. çıkartmak istemediğimizde burası -1 diğerleri -1'siz oluyor.
+
+                for (int i = 0; i < arr.Length-1; i++)
                 {
                     if (i % 2 == 0)
                     {
                         orderderArray[s] = arr[i];
                         s++;
                     }
+                  
+                    
                     else
                     {
                         orderderArray[e] = arr[i];
@@ -29,7 +32,6 @@ namespace Numbers
 
                 return orderderArray;
             }
-
             int[] sample1 = { 4, 13, 8, 9, 7, 1, 6 };
             int[] sample2 = { 16, 23, 7, 11, 3, 14 };
             var result1 = OrderArrayForStartToEnd(sample1);
@@ -37,9 +39,10 @@ namespace Numbers
             var resultText = string.Join(",", result1);
             var resultText2 = string.Join(",", result2);
 
-
             Console.WriteLine(resultText);
+          
             Console.WriteLine(resultText2);
+           
             Console.ReadLine();
         }
     }
